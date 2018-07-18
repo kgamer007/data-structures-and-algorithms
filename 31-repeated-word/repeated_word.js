28f316bbf846f32528464ce3150f28d98465e904
Kevin Hwangpo <hwangpo3@gmail.com>
2018-07-18 14:30:54 -0700

    functional..

--- 31-repeated-word/repeated_word.js
+++ 31-repeated-word/repeated_word.js
@@ -4,15 +4,16 @@ let string = 'This will test the repeated word, this will work.';
 const repeatedWord = (str) => {
   const map = {};
   let output = null;
-  string = str.replace(/\./g, ' ');
-  console.log(string);
+  string = str.replace(/[[a-z], [A-Z], \.,] /g, ' ');
+ 
   let i = 0;
   while (output === null) {
     const split = string.split(' ');
-    map[split[i]] === 'undefined' ? map[split[i]] = 1 : map[split[i]]++;
-    console.log(map);
+    map[split[i]] === undefined ? map[split[i]] = 1 : map
+      [split[i]]++;
     map[split[i]] === 2 ? output = split[i] : i++;
+    console.log(map);
   }
-  console.log(output);
+  return output;
 };
 repeatedWord(string);
